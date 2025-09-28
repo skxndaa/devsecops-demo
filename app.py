6851cacdaff543d64251f800ed8e0227e3a94437
skxndaa <skandaks18@gmail.com>
2025-09-28 16:30:37 +0530

    Initial commit with Flask application

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """A simple endpoint that returns a greeting."""
+    return "Hello, DevSecOps!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
